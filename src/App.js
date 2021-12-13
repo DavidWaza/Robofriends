7db78e41d41593de7c75e3ee063bc533d6d4b0c5
DavidWaza <wazabanga00@gmail.com>
2021-12-13 19:24:19 +0100

    added scroll to navigation and font-family

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import 'tachyons';
-import { robots } from './robots'
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>  response.json())
+    .then(users => this.setState({ robots: users}));
   }
   onSearchChange = (e) => {
     this.setState({searchfield : e.target.value});
@@ -20,14 +26,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-        console.log(filteredRobots);
+   if( this.state.robots.length === 0){
+     return <h1 className='tc'>Loading.....</h1>
+   } else {
     return (
     <div className="App">  
     <h1 className='f1 tc'>RoboFriends</h1>
      <SearchBox  searchChange={this.onSearchChange}/>
+     <Scroll>
    <CardList  robots={filteredRobots}/>
+   </Scroll>
     </div>
   );
+   }
+    
 }
   }
 
